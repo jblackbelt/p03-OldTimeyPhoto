d437604a2e5ac3b2b319315bd9492b659b49fecd
jblackbelt <jwest27.mail.csuchico.edu>
2017-10-02 16:41:25 -0700

    basic work

--- p03.cpp
+++ p03.cpp
@@ -7,10 +7,25 @@ using namespace std;
 
 int main()
 {
-  Bitmap image;
-  vector <vector <Pixle>> bmp;
-  
-  
-
-  return 0;
+    string pic;
+    Bitmap image;
+    vector <vector <Pixle>> bmp;
+    int loop = 0;
+    while(loop == 0)
+    { 
+        cout<<"please imput a 24 bit depth Windows BMP file of a picture." << endl;
+        cin >> pic;
+        image.open(pic);
+        bmp = image.toPixelMatrix();
+        if (bmp.isImage())
+        {
+            loop = 1;
+        }
+        else
+        {
+            cout << "Error ";
+        }
+    }
+    
+    return 0;
 }
